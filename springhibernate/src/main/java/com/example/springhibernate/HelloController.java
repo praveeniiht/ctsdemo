d8f48a1788444d7ef4fa4b647f60f4ddd0fe91fa
HP <HP@Praveen-hp>
2021-05-19 10:52:03 +0530

    Hibernate Commit message

---
+++ springhibernate/src/main/java/com/example/springhibernate/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.springhibernate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/test")
+	public String test() {
+		return "Hello, I am working";
+	}
+
+}
---
+++ springhibernate/src/main/java/com/example/springhibernate/SpringhibernateApplication.java
@@ -0,0 +1,14 @@
+package com.example.springhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication(scanBasePackages="com.example.springhibernate")
+public class SpringhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringhibernateApplication.class, args);
+	}
+
+}
