e8f4a218904a14ad51ccc2c16b194fb215ef137c
HP <HP@Praveen-hp>
2021-05-19 10:54:46 +0530

    Added another post method for teting

--- springhibernate/src/main/java/com/example/springhibernate/HelloController.java
+++ springhibernate/src/main/java/com/example/springhibernate/HelloController.java
@@ -2,6 +2,7 @@ package com.example.springhibernate;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +10,11 @@ public class HelloController {
 	
 	@GetMapping("/test")
 	public String test() {
-		return "Hello, I am working";
+		return "Hello";
 	}
 
+	@PostMapping("/test1")
+	public String test1() {
+		return "bye";
+	}
 }
